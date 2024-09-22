0151837c49268a8c26009d9622b317c4d6505573
Hasip Timurtas <hasip.timurtas@gmail.com>
2024-09-22 18:03:02 +0200

    refactor: Update OwnedNfts component to improve NFT image loading and error handling

--- src/components/OwnedNfts.vue
+++ src/components/OwnedNfts.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <h2 id="ownedNftsTitle">Your NFTs</h2>
-        <button @click="$emit('load-nfts')">Load My NFTs</button>
+        <button @click="loadNFTs">Load My NFTs</button>
         <div class="owned-nfts" v-if="nfts && nfts.length > 0">
             <div class="nft" v-for="nft in nfts" :key="nft.tokenId">
-                <img :src="nft.tokenURI" />
+                <img :src="nft.tokenURI" :alt="`NFT Image ${nft.tokenId}`" @error="onImageError" />
                 <p>Token ID: {{ nft.tokenId }}</p>
-                <button @click="$emit('transfer-nft', nft.tokenId)">Transfer</button>
+                <button @click="transferNFT(nft.tokenId)">Transfer</button>
             </div>
         </div>
         <p v-else>No NFTs found for this wallet.</p>
@@ -15,7 +15,41 @@
 
 <script>
 export default {
-    props: ["nfts"],
+    /**
+     * Component to display the user's owned NFTs.
+     * @component
+     * @prop {Array} nfts - Array of NFT objects with tokenId and tokenURI.
+     * @emits load-nfts - Emitted when the user clicks the 'Load My NFTs' button.
+     * @emits transfer-nft - Emitted when the user clicks the 'Transfer' button for an NFT.
+     */
+    props: {
+        nfts: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    methods: {
+        /**
+         * Emits the 'load-nfts' event to load NFTs.
+         */
+        loadNFTs() {
+            this.$emit('load-nfts');
+        },
+        /**
+         * Emits the 'transfer-nft' event with the specified tokenId.
+         * @param {string|number} tokenId - The ID of the token to transfer.
+         */
+        transferNFT(tokenId) {
+            this.$emit('transfer-nft', tokenId);
+        },
+        /**
+         * Handles image load errors by setting a placeholder image.
+         * @param {Event} event - The error event from the image.
+         */
+        onImageError(event) {
+            event.target.src = 'path/to/placeholder/image.png'; // Replace with your placeholder image path
+        },
+    },
 };
 </script>
 
